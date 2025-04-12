824ee1051a07ea3dc536746515b98d900cdec0a3
Leandro Struni <164394186+LeanEmanuel@users.noreply.github.com>
2025-04-12 19:18:48 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nba-rookie-app-react',
+  webDir: 'dist'
+};
+
+export default config;
